86bf53040cded70c18ba0951fc77f68b5309b602
komed3 <pk97@outlook.de>
2026-05-10 11:51:48 +0200

    export dim vectors as const

---
+++ src/types/dimension.ts
@@ -0,0 +1,35 @@
+export type Dimension = readonly [
+  T: number, // time
+  L: number, // length
+  M: number, // mass
+  I: number, // electric current
+  θ: number, // temperature
+  N: number, // amount of substance
+  J: number  // luminous intensity
+];
+
+// Dimensionless
+export const ScalarDim = [ 0, 0, 0, 0, 0, 0, 0 ] as const satisfies Dimension;
+export type ScalarDim = typeof ScalarDim;
+
+// Base Dimensions
+export const TimeDim = [ 1, 0, 0, 0, 0, 0, 0 ] as const satisfies Dimension;
+export type TimeDim = typeof TimeDim;
+
+export const LengthDim = [ 0, 1, 0, 0, 0, 0, 0 ] as const satisfies Dimension;
+export type LengthDim = typeof LengthDim;
+
+export const MassDim = [ 0, 0, 1, 0, 0, 0, 0 ] as const satisfies Dimension;
+export type MassDim = typeof MassDim;
+
+export const ElectricCurrentDim = [ 0, 0, 0, 1, 0, 0, 0 ] as const satisfies Dimension;
+export type ElectricCurrentDim = typeof ElectricCurrentDim;
+
+export const TemperatureDim = [ 0, 0, 0, 0, 1, 0, 0 ] as const satisfies Dimension;
+export type TemperatureDim = typeof TemperatureDim;
+
+export const AmountOfSubstanceDim = [ 0, 0, 0, 0, 0, 1, 0 ] as const satisfies Dimension;
+export type AmountOfSubstanceDim = typeof AmountOfSubstanceDim;
+
+export const LuminousIntensityDim = [ 0, 0, 0, 0, 0, 0, 1 ] as const satisfies Dimension;
+export type LuminousIntensityDim = typeof LuminousIntensityDim;
