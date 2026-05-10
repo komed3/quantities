64d32bdfa01b698ba0dc99ee0e3183715dbec0b7
komed3 <pk97@outlook.de>
2026-05-10 11:36:58 +0200

    fix import paths

--- src/def/prefix/neg/nano.ts
+++ src/def/prefix/neg/nano.ts
@@ -1,4 +1,4 @@
-import type { PrefixDef, PrefixRef } from '../../../types/prefix';
+import type { PrefixDef, PrefixRef } from '../../../types/def/prefix';
 
 export const nano = 'n' as PrefixRef< 'n' >;
 
