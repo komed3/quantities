2d0a80571bb7cef7f36bb80c35fca05cf23effa0
komed3 <pk97@outlook.de>
2026-05-11 23:00:24 +0200

    declare mass quantity

---
+++ src/def/quantity/base/mass.ts
@@ -0,0 +1,31 @@
+import type { QuantityDef, QuantityRef } from '../../../types/def/quantity';
+import { Branch, Format, Lang } from '../../../types/dict';
+import { MassDim } from '../../../types/dimension';
+
+export const mass = 'mass' as QuantityRef< MassDim >;
+
+export default ( {
+  id: mass,
+  dim: LengthDim,
+  units: [],
+  baseUnit: undefined,
+  branch: Branch.COMMON,
+  meta: {
+    symbol: [ {
+      id: 'm',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 'm',
+        [ Format.LATEX ]: 'm'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: 'mass',
+      [ Lang.DE ]: 'Masse'
+    },
+    description: {
+      [ Lang.EN ]: 'quantity expressing the amount of matter in a physical body',
+      [ Lang.DE ]: 'Größe, die die Menge an Materie in einem physischen Körper ausdrückt'
+    }
+  }
+} ) as const satisfies QuantityDef< MassDim, typeof mass >;
