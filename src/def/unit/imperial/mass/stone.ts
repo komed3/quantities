c36e5812bb18d33c935c8c3e2a5f8236d3abb257
komed3 <pk97@outlook.de>
2026-05-12 02:03:05 +0200

    stone will not have plural

--- src/def/unit/imperial/mass/stone.ts
+++ src/def/unit/imperial/mass/stone.ts
@@ -35,7 +35,7 @@ export default ( {
       }
     } ],
     name: {
-      [ Lang.EN ]: [ 'stone', 'stones' ],
+      [ Lang.EN ]: [ 'stone' ],
       [ Lang.DE ]: [ 'Stone' ]
     },
     description: {
