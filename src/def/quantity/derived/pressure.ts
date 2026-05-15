7e04adb0f7f64579b1656630a69d2e7d12fdae31
komed3 <pk97@outlook.de>
2026-05-15 18:26:31 +0200

    implement pressure quantity

---
+++ src/def/quantity/derived/pressure.ts
@@ -0,0 +1,35 @@
+import type { QuantityDef, QuantityRef } from '../../../types/def/quantity';
+import { Branch, Format, Lang } from '../../../types/dict';
+import { PressureDim } from '../../../types/dimension';
+
+import { pascal } from '../../unit/si/derived/pascal';
+
+export const pressure = 'pressure' as QuantityRef< PressureDim >;
+
+export default ( {
+  id: pressure,
+  dim: PressureDim,
+  units: [
+    pascal
+  ],
+  baseUnit: pascal,
+  branch: Branch.MECHANICS,
+  meta: {
+    symbol: [ {
+      id: 'p',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 'p',
+        [ Format.LATEX ]: 'p'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: 'pressure',
+      [ Lang.DE ]: 'Druck'
+    },
+    description: {
+      [ Lang.EN ]: 'quantity expressing the force applied perpendicular to the surface of an object per unit area',
+      [ Lang.DE ]: 'Größe, die die auf eine Oberfläche senkrecht wirkende Kraft pro Flächeneinheit angibt'
+    }
+  }
+} ) as const satisfies QuantityDef< PressureDim, typeof pressure >;
