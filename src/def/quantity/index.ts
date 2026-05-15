b8bf10b64b53a4fdd4681567e22ba7314c23a1f1
komed3 <pk97@outlook.de>
2026-05-16 01:33:15 +0200

    mount SI base quantities

--- src/def/quantity/index.ts
+++ src/def/quantity/index.ts
@@ -1,3 +1,14 @@
 import type { QuantityRegistry } from '../../types/def/registry';
 
-export default ( [] ) as const satisfies QuantityRegistry;
+import { amountOfSubstance } from './base/amountOfSubstance';
+import { electricCurrent } from './base/electricCurrent';
+import { length } from './base/length';
+import { luminousIntensity } from './base/luminousIntensity';
+import { mass } from './base/mass';
+import { temperature } from './base/temperature';
+import { time } from './base/time';
+
+export default ( [
+  // SI base quantities
+  time, length, mass, electricCurrent, temperature, amountOfSubstance, luminousIntensity
+] ) as const satisfies QuantityRegistry;
