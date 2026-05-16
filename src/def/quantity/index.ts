97fa58dc22d3a42d1da0e276bbfaaab3887dd4af
komed3 <pk97@outlook.de>
2026-05-16 15:46:53 +0200

    register all declares quantities

--- src/def/quantity/index.ts
+++ src/def/quantity/index.ts
@@ -8,7 +8,28 @@ import { mass } from './base/mass';
 import { temperature } from './base/temperature';
 import { time } from './base/time';
 
+import { angle } from './derived/angle';
+import { force } from './derived/force';
+import { frequency } from './derived/frequency';
+import { pressure } from './derived/pressure';
+import { solidAngle } from './derived/solidAngle';
+import { stress } from './derived/stress';
+
 export default ( [
   // SI base quantities
-  time, length, mass, electricCurrent, temperature, amountOfSubstance, luminousIntensity
+  amountOfSubstance,
+  electricCurrent,
+  length,
+  luminousIntensity,
+  mass,
+  temperature,
+  time,
+
+  // derived quantities
+  angle,
+  force,
+  frequency,
+  pressure,
+  solidAngle,
+  stress
 ] ) as const satisfies QuantityRegistry;
